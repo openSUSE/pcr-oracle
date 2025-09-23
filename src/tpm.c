b2cc8722c680096e055a00ae0d09d1028467f812
Gary Lin <glin@suse.com>
2025-09-23 14:46:40 +0800

    Extra checks for TPM self-test
    Do a few more checks besides the TPM SelfTest command
    - Check the TPM properties: PropertyStartupClear and PropertyPermanent
    - Check the tagged policies associated with the following permanent
      handles: TPM2_RH_LOCKOUT, TPM2_RH_OWNER, and TPM2_RH_ENDORSEMENT
    
    Signed-off-by: Gary Lin <glin@suse.com>

--- src/tpm.c
+++ src/tpm.c
@@ -85,14 +85,199 @@ tss_esys_context(void)
 	return esys_ctx;
 }
 
+static bool
+tpm_get_tpm_property(TPM2_PT property, uint32_t *value)
+{
+	ESYS_CONTEXT *esys_ctx = tss_esys_context();
+	TPMS_CAPABILITY_DATA *cap_data = NULL;
+	TPMI_YES_NO more_data;
+	TPML_TAGGED_TPM_PROPERTY *props = NULL;
+	TSS2_RC rc;
+	bool okay = false;
+
+	if (value == NULL)
+		return false;
+
+	rc = Esys_GetCapability(esys_ctx, ESYS_TR_NONE, ESYS_TR_NONE,
+			ESYS_TR_NONE, TPM2_CAP_TPM_PROPERTIES, property,
+			1, &more_data, &cap_data);
+	if (rc != TSS2_RC_SUCCESS)
+		return tss_check_error(rc, "Esys_GetCapability (TPM_PROPERTIES) failed");
+
+	if (cap_data == NULL) {
+		error("Empty CAP data (TPM_PROPERTIES)\n");
+		return false;
+	}
+
+	if (cap_data->capability != TPM2_CAP_TPM_PROPERTIES) {
+		error("Wrong CAP data (TPM_PROPERTIES)\n");
+		goto out;
+	}
+
+	props = &cap_data->data.tpmProperties;
+	if (props->count != 1) {
+		error("Got more than 1 property\n");
+		goto out;
+	}
+
+	if (props->tpmProperty[0].property != property) {
+		error("Property not match\n");
+		goto out;
+	}
+
+	*value = props->tpmProperty[0].value;
+
+	okay = true;
+out:
+	if (cap_data)
+		free(cap_data);
+
+	return okay;
+}
+
+#ifdef TPM2_CAP_AUTH_POLICIES
+static bool
+tpm_get_auth_policies(TPM2_HANDLE hierarchy, TPMS_CAPABILITY_DATA **cap_data)
+{
+	ESYS_CONTEXT *esys_ctx = tss_esys_context();
+	TPMI_YES_NO more_data;
+	TSS2_RC rc;
+
+	rc = Esys_GetCapability(esys_ctx, ESYS_TR_NONE, ESYS_TR_NONE,
+			ESYS_TR_NONE, TPM2_CAP_AUTH_POLICIES, hierarchy,
+			TPM2_MAX_TAGGED_POLICIES, &more_data, cap_data);
+	if (rc != TSS2_RC_SUCCESS)
+		return tss_check_error(rc, "Esys_GetCapability (AUTH_POLICIES) failed");
+
+	if (*cap_data == NULL) {
+		error("Empty CAP data (AUTH_POLICIES)\n");
+		return false;
+	}
+
+	if ((*cap_data)->capability != TPM2_CAP_AUTH_POLICIES) {
+		error("Wrong CAP data (AUTH_POLICIES)\n");
+		return false;
+	}
+
+	return true;
+}
+
+static bool
+tpm_check_auth_policies(TPM2_HANDLE hierarchy)
+{
+	TPMS_CAPABILITY_DATA *cap_data = NULL;
+	TPML_TAGGED_POLICY *policies = NULL;
+	uint32_t i;
+	bool okay = false;
+
+	if (!tpm_get_auth_policies(hierarchy, &cap_data))
+		goto out;
+
+	policies = &cap_data->data.authPolicies;
+	for (i = 0; i < policies->count; i++) {
+		if (policies->policies[i].policyHash.hashAlg != TPM2_ALG_NULL) {
+			error("Tagged policy NON-NULL Hash Algorithm\n");
+			goto out;
+		}
+	}
+
+	okay = true;
+out:
+	if (cap_data)
+		free(cap_data);
+
+	return okay;
+}
+#endif
+
+static bool
+tpm_check_capabilities(void)
+{
+	uint32_t prop_startup, prop_permanent;
+
+	/* Check PropertyStartupClear (TPM2_PT_STARTUP_CLEAR) */
+	if (!tpm_get_tpm_property(TPM2_PT_STARTUP_CLEAR, &prop_startup)) {
+		error("Failed to get PropertyStartupClear\n");
+		return false;
+	}
+
+	if ((prop_startup & TPMA_STARTUP_CLEAR_SHENABLE) == 0) {
+		error("Storage hierarchy not enabled\n");
+		return false;
+	}
+
+	if ((prop_startup & TPMA_STARTUP_CLEAR_EHENABLE) == 0) {
+		error("Endorsement hierarchy not enabled\n");
+		return false;
+	}
+
+	/* Check PropertyPermanent (TPM2_PT_PERMANENT) */
+	if (!tpm_get_tpm_property(TPM2_PT_PERMANENT, &prop_permanent)) {
+		error("Failed to get PropertyPermanent\n");
+		return false;
+	}
+
+	if ((prop_permanent & TPMA_PERMANENT_OWNERAUTHSET) != 0) {
+		error("TPM2 Owner Authorization set\n");
+		return false;
+	}
+
+	if ((prop_permanent & TPMA_PERMANENT_ENDORSEMENTAUTHSET) != 0) {
+		error("TPM2 Endorsement Authorization set\n");
+		return false;
+	}
+
+	if ((prop_permanent & TPMA_PERMANENT_LOCKOUTAUTHSET) != 0) {
+		error("TPM2 Lockout Authorization set\n");
+		return false;
+	}
+
+	if ((prop_permanent & TPMA_PERMANENT_INLOCKOUT) != 0) {
+		error("TPM2 in lockout\n");
+		return false;
+	}
+
+#ifdef TPM2_CAP_AUTH_POLICIES
+	/*
+	 * Ensure that there is no authorization policy associated with the
+	 * following hierarchies: TPM2_RH_LOCKOUT, TPM2_RH_OWNER, and
+	 * TPM2_RH_ENDORSEMENT
+	 */
+	if (!tpm_check_auth_policies(TPM2_RH_LOCKOUT)) {
+		error("Error from LockOut handle\n");
+		return false;
+	}
+
+	if (!tpm_check_auth_policies(TPM2_RH_OWNER)) {
+		error("Error from Owner handle\n");
+		return false;
+	}
+
+	if (!tpm_check_auth_policies(TPM2_RH_ENDORSEMENT)) {
+		error("Error from Endorsement handle\n");
+		return false;
+	}
+#endif
+
+	return true;
+}
+
 bool
 tpm_selftest(bool fulltest)
 {
 	ESYS_CONTEXT *esys_ctx = tss_esys_context();
 	TSS2_RC rc;
 
+	/* TPM self test */
 	rc = Esys_SelfTest(esys_ctx, ESYS_TR_NONE, ESYS_TR_NONE, ESYS_TR_NONE, fulltest);
-	return tss_check_error(rc, "TPM self test failed");
+	if (rc != TSS2_RC_SUCCESS)
+		return tss_check_error(rc, "TPM self test failed");
+
+	/* Capability check */
+	if(!tpm_check_capabilities())
+		return false;
+
+	return true;
 }
 
 bool
